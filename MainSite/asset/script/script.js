e39cafceeb81b546ae8d2e95f0eaa787d09657c4
Mr A <rehaanclasses@gmail.com>
2023-05-09 04:07:04 +0530

    Responsive Menu Complete

--- MainSite/asset/script/script.js
+++ MainSite/asset/script/script.js
@@ -4,11 +4,17 @@ dropBtn.addEventListener('click', dropMenu);
 
 function dropMenu(){
     let menuHolder = document.querySelector(".menu-list");
+    let links = document.querySelector('.links');
+    
     if(menuHolder.style.height != "82.2vh"){
         menuHolder.style.height = "82.2vh";
         dropBtn.style.transform = "rotate(180deg)";
+        links.style.transform = 'scale(1)';
+
     }else{
         menuHolder.style.height = "80px";
         dropBtn.style.transform = "rotate(0deg)";
+        links.style.transform = 'scale(0)';
+
     }
 };
